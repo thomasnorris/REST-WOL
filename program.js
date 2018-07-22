5f49439823a694d63eb086723f95642c666e320b
thomasnorris <tom.norris135@gmail.com>
2018-07-22 19:28:28 -0400

    make endpoint names constants; remove devices constant

--- program.js
+++ program.js
@@ -1,13 +1,17 @@
 (function() {
     var _wol = require('wake_on_lan');
     var _express = require('express')();
+    _express.set('json spaces', 4);
 
     const PORT = 1000;
-    const TOSHIBA = new Device('Laptop', 'Toshiba Laptop', '7C:05:07:13:B7:BF');
+    const WAKE_ENDPOINT = '/wake/:name?';
+    const DEVICES_ENDPOINT = '/devices';
 
-    var _devices = [TOSHIBA];
+    var _devices = [
+        new Device('Laptop', 'Toshiba Laptop', '7C:05:07:13:B7:BF')
+    ];
 
-    _express.get('/wake/:name?', (req, res) => {
+    _express.get(WAKE_ENDPOINT, (req, res) => {
         var name = req.params.name;
         if (name === undefined) {
             res.send('Device name not supplied.\n/wake/{name}');
@@ -15,11 +19,11 @@
         }
 
         var device = _devices.filter((device) => {
-            return device.nickName.toLowerCase() === name.toLowerCase();
+            return device.name.toLowerCase() === name.toLowerCase();
         })[0];
 
         if (device === undefined) {
-            res.send('Device with nickname ' + name + ' not found.');
+            res.send('Device with name \"' + name + '\" not found.');
             return;
         }
 
@@ -27,15 +31,19 @@
             if (err)
                 res.send('There was a WOL error: ' + err);
             else
-                res.send('WOL packet sent to ' + device.fullName + ' at address ' + device.mac);
+                res.send('WOL packet sent to \"' + device.desc + '\" at address \"' + device.mac + '\".');
         });
+    });
+
+    _express.get(DEVICES_ENDPOINT, (req, res) => {
+        res.json(_devices);
     });
 
     _express.listen(PORT);
 
-    function Device(nickName, fullName, mac) {
-        this.nickName = nickName;
-        this.fullName = fullName;
+    function Device(name, desc, mac) {
+        this.name = name;
+        this.desc = desc;
         this.mac = mac;
     }
 })();
