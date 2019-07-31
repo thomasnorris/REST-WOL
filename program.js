21384a71099af08ae0347199307afba27491acda
Thomas Norris <tom.norris135@gmail.com>
2019-07-31 16:39:14 -0400

    add .gitignore; remove node_modules folder

--- program.js
+++ program.js
@@ -1,8 +1,9 @@
 (function() {
     var _wol = require('wake_on_lan');
     var _express = require('express')();
-    _express.set('json spaces', 4);
+    var _fs = require('fs');
 
+    const LOG_FILE_PATH = __dirname + '/WOL-Logs.txt';
     const PORT = 1000;
     const WAKE_ENDPOINT = '/wake/:name?/:customResponse?';
     const DEVICES_ENDPOINT = '/devices';
@@ -11,6 +12,8 @@
         new Device('Server', 'Laptop Server', '7C:05:07:13:B7:BF', '192.168.0.10'),
         new Device('Desktop', 'Leviathan', '70:8B:CD:4E:33:6A', '192.168.0.20')
     ];
+
+    _express.set('json spaces', 4);
 
     _express.get(WAKE_ENDPOINT, (req, res) => {
         res.set('Content-Type', 'text/html');
@@ -32,8 +35,12 @@
         }
 
         _wol.wake(device.mac, (err) => {
-            if (err)
-                res.send('There was a WOL error: ' + err);
+            var logText = 'WOL packet sent at: ' + new Date().toISOString().slice(0,10) + '\n-- Device: \"' + device.desc + '\"';
+            if (err) {
+                var errText = 'There was a WOL error: ' + err;
+                res.send(errText);
+                logText += '\n' + errText;
+            }
             else {
                 var customResponse = req.params.customResponse;
                 if (customResponse)
@@ -41,6 +48,8 @@
                 else
                     res.send('<div>WOL packet sent to \"' + device.desc + '\" with IP \"' + device.ip + '\" and MAC address \"' + device.mac + '\".</div>');
             }
+
+            log(logText);
         });
     });
 
@@ -55,5 +64,16 @@
         this.desc = desc;
         this.mac = mac;
         this.ip = ip;
+    }
+
+    function log(logText) {
+        _fs.stat(LOG_FILE_PATH, (err, stats) => {
+            if (!stats || stats.size === 0)
+                _fs.closeSync(_fs.openSync(LOG_FILE_PATH, 'w'));
+
+            var stream = _fs.createWriteStream(LOG_FILE_PATH, { flags: 'a' });
+            stream.write(logText);
+            stream.end('\n');
+        })
     }
 })();
