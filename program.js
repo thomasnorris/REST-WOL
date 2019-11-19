0fc8b9b954d5580e0298d913ffc83dc59166ad11
Thomas Norris <tom.norris135@gmail.com>
2019-11-19 09:53:30 -0500

    consts out of order

--- program.js
+++ program.js
@@ -3,12 +3,13 @@
     var _wol = require('wake_on_lan');
     var _express = require('express');
     var _app = _express();
-    var _devices = SETTINGS.Devices;
-    var _endpoints = SETTINGS.Endpoints;
 
     const LISTEN_PORT = 1000;
     const SETTINGS = readJson(_path.resolve(__dirname, 'settings.json'));
     const MC_SERVER_RESPONSE_PARAM = 'mc';
+
+    var _devices = SETTINGS.Devices;
+    var _endpoints = SETTINGS.Endpoints;
 
     _app.get(_endpoints.Wake, (req, res) => {
         res.set('Content-Type', 'text/html');
